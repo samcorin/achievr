c85ae9809eb7900e2d5e7c58c3a93aae6981a5b9
Nicolas Duchemin <duchemin.nicolas@gmail.com>
2017-04-18 14:04:02 +0900

    Work in progress seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,46 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+User.destroy_all
+Team.destroy_all
+Company.destroy_all
+puts "Cleaning database..."
+
+
+company = Company.new(
+    name: 'Indeed.com',
+    mission: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+    profile: 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.',
+    product: 'At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.',
+)
+company.save!
+
+team = Team.create!(
+  name: 'Sales Team',
+  company: company
+)
+
+puts 'Creating user...'
+    User.create(
+    first_name: 'Nicolas',
+    last_name: 'Duchemin',
+    points: 0,
+    email: 'nicolas@indeed.com',
+    password: '123123',
+    photo: "https://lorempixel.com/200/200/business",
+    bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+    team: team
+  )
+
+
+resource = Resource.new(
+  name: 'Sales Training',
+  picture: 'https://lorempixel.com/200/200/business',
+  url: 'https://www.slideshare.net/cemara288/basic-sales-training',
+)
+  resource.team = team
+  resource.save!
+
+puts 'Finished!'
