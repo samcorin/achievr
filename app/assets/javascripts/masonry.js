cb416bd7f5001a0dc76775bda2956dc065a169a8
Hidehiro Nagaoka <kuinige@gmail.com>
2017-04-19 14:10:26 +0900

    added the video to the welcome page

---
+++ app/assets/javascripts/masonry.js
@@ -0,0 +1,19 @@
+// $(document).ready(function() {
+//   // Masonry grid setup
+//   $(".grid").masonry({
+//     itemSelector: ".grid-item",
+//     columnWidth: ".grid-item",
+//     gutter: 15,
+//     percentPosition: true
+//   });
+
+//   // Image replacement handler
+//   // $(document).on("click", ".js-button", function() {
+//   //   var imageSrc = $(this).parents(".grid__item").find("img").attr("src");
+//   //   $(".js-download").attr("href", imageSrc);
+//   //   $(".js-modal-image").attr("src", imageSrc);
+//   //   $(document).on("click", ".js-heart", function() {
+//   //     $(this).toggleClass("active");
+//   //   });
+//   // });
+// })
