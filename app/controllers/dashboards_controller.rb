f966a3d5f3d2049ba15b9293b27193631cd46e1f
Sam Corin <samcorin@gmail.com>
2017-04-19 16:55:31 +0900

    add react test component

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,5 +1,6 @@
 class DashboardsController < ApplicationController
   def show
+    @users = User.all
     @user = current_user
     redirect_to company_welcome_company_path if current_user.sign_in_count == 1
   end
---
+++ client/app/bundles/Cards/startup/registration.jsx
@@ -0,0 +1,8 @@
+import ReactOnRails from 'react-on-rails';
+
+import Cards from '../components/Cards';
+
+// This is how react_on_rails can see the Cards in the browser.
+ReactOnRails.register({
+  Cards,
+});
