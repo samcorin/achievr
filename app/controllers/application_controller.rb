66be18dc727a81e9a813d3044f63394a57bfb612
Nicolas Duchemin <duchemin.nicolas@gmail.com>
2017-04-24 16:02:56 +0900

    Set up Open Graph, Twitter, and meta tag

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,3 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
 end
+
+def default_url_options
+  { host: ENV['http://www.achievr.io/'] || 'localhost:3000' }
+end
