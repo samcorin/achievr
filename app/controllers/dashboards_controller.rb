b3e858f1d82e5bbc08fc17a6a8baacfd21678a50
Sam Corin <samcorin@gmail.com>
2017-04-26 14:53:42 +0900

    improved user flow

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -10,5 +10,7 @@ class DashboardsController < ApplicationController
     @badges = current_user.badges
 
     @team = current_user.team
+
+    @email = "mailto:#{(@team.users.pluck(:email) - [current_user.email]).join(',')}?Subject=Nice%20to%20meet%20you&body=Hi%20everyone,%0D%0AI%20look%20forward%20to%20meeting%20you%20all%20on%20Monday."
   end
 end
