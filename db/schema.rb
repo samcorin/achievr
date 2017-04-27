28a03a0d4e26844dcc17e4e03ee92a5fecb8e016
Nicolas Duchemin <duchemin.nicolas@gmail.com>
2017-04-27 10:41:33 +0900

    Generated a migration to add the description column into Teams

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170427012829) do
+ActiveRecord::Schema.define(version: 20170427014021) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -80,8 +80,9 @@ ActiveRecord::Schema.define(version: 20170427012829) do
   create_table "teams", force: :cascade do |t|
     t.string   "name"
     t.integer  "company_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.text     "description"
     t.index ["company_id"], name: "index_teams_on_company_id", using: :btree
   end
 
